e7040995c36c784b794fe7eb8c4e2a59c0317b35
ermwhatesigma <ermwhatesigma420@gmail.com>
2025-11-02 15:33:15 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -40,19 +40,22 @@ while True:
     print(GREEN + "Maker : Sigma")
     print("Github : https://github.com/ermwhatesigma" + RESET)
     print("\n1. Deep search a name")
-    print("2. Phone number and email scraper **note that it might work but give the wrong number or email**")
-    print("3. AI chat. All the conted generated after selecting this choice is AI generated")
-    print("4. Exit")
+    print("2. Phone number and email scraper **note** : That it might work but give the wrong number or email")
+    print("3. NOTHING")
+    print("4. AI chat. All the conted generated after selecting this choice is AI generated. It is still experimental so it might not work.")
+    print("5. Exit")
 
-    choice = input("Choose an option (1-4): ")
+    choice = input("Choose an option (1-5): ")
 
     if choice == '1':
         run_script('scripts\\name.py')
     elif choice == '2':
         run_script('scripts\\phone-email.py')
     elif choice == '3':
-        run_script('scripts\\gpt.py')
+        run_script('scripts\\phone.py')
     elif choice == '4':
+        run_script('scripts\\gpt.py')
+    elif choice == '5':
         run_script('scripts\\clear.py')
         print("WHY DID YOU LEAVE?")
         quit()
